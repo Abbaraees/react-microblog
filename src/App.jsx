68a9850d2b04d0bed7083f7cdd64d0f55359cfe6
Muhammad Lawal <abbaraees@gmail.com>
2024-02-06 10:06:20 +0100

    feat: add dependencies, setup react-bootsrap, and create some initial components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,39 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Container from 'react-bootstrap/Container';
+import Header from './components/Header';
+import { Stack } from 'react-bootstrap';
+import Sidebar from './components/Sidebar';
+import Posts from './components/Posts';
+import Body from './components/Body';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const posts = [
+    {
+      id: 1,
+      text: 'Hello, world!',
+      timestamp: 'a minute ago',
+      author: {
+        username: 'susan',
+      },
+    },
+    {
+      id: 2,
+      text: 'Second post',
+      timestamp: 'an hour ago',
+      author: {
+        username: 'john',
+      },
+    },
+  ];
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container fluid className="App"> 
+      <Header />
+      <Body sidebar>
+        <Posts />
+      </Body>
+    </Container >
+  );
 }
 
 export default App
